337666fcff3f433076e13fc3f0c23edf9f9b4118
Bucephalus <bhargabnath691@gmail.com>
2021-03-19 16:08:57 +0530

    somw bugs removed from disaster cintroller

--- controller/disaster.js
+++ controller/disaster.js
@@ -23,68 +23,71 @@ exports.all_locations_get = async (req, res) => {
 }
 
 exports.compare_distance = async (req, res) => {
-    const disasters = await Disaster.find().select('coordinates');
-    const userLoc = await User.find().select('default_loc');
+    try {
+        const disasters = await Disaster.find().select('coordinates');
+        const userLoc = await User.find().select('default_loc');
 
-    const origins = [];
-    const destinations = [];
+        const origins = [];
+        const destinations = [];
 
-    userLoc.map(uLoc => {
-        origins.push(uLoc.default_loc);
-    });
+        userLoc.map(uLoc => {
+            origins.push(uLoc.default_loc);
+        });
 
-    disasters.map(disaster => {
-        destinations.push(disaster.coordinates);
-    });
+        disasters.map(disaster => {
+            destinations.push(disaster.coordinates);
+        });
 
-    distance.key('AlphaDMABzEE8y03t0VsipsvR1z7fkZDiU7Erqmy');
-    distance.units('metric');
-    const userLocations = [];
-    const userLocationsArray = [];
+        distance.key('AlphaDMACMzobKdnQbF9WWL0han6Bjv6rcQqv19H');
+        distance.units('metric');
+        const userLocations = [];
+        const userLocationsArray = [];
 
-    distance.matrix(origins, destinations, async (err, distances) => {
-        if (err) {
-            return res.json({ error: 'Something went wrong!' });
-        }
-        if (!distances) {
-            return res.json({ error: 'Distance not measurable!' });
-        }
-        if (distances.status == 'OK') {
-            for (let i = 0; i < origins.length; i++) {
-                for (let j = 0; j < destinations.length; j++) {
-                    if (distances.rows[0].elements[j].status == 'OK') {
-                        const distance = distances.rows[i].elements[j].distance.text;
-                        const result = distance.split(' ');
-                        if (result[0] > 10) {
-                            userLocations.push(origins[i]);
-                            const users = await User.find({
-                                default_loc: origins[i],
-                                role: { $ne: 1 }
-                            });
-                            userLocationsArray.push(users[0]);
+        distance.matrix(origins, destinations, async (err, distances) => {
+            if (err) {
+                return res.json({ error: 'Something went wrong!' });
+            }
+            if (!distances) {
+                return res.json({ error: 'Distance not measurable!' });
+            }
+            if (distances.status == 'OK') {
+                for (let i = 0; i < origins.length; i++) {
+                    for (let j = 0; j < destinations.length; j++) {
+                        if (distances.rows[0].elements[j].status == 'OK') {
+                            const distance = await distances.rows[i].elements[j].distance.text;
+                            const result = await distance.split(' ');
+                            if (result[0] > 10) {
+                                userLocations.push(origins[i]);
+                                const users = await User.find({
+                                    default_loc: origins[i],
+                                    role: { $ne: 1 }
+                                });
+                                userLocationsArray.push(users[0]);
+                            }
+                        } else {
+                            console.log('Not reachable!');
                         }
-                    } else {
-                        console.log('Not reachable!');
                     }
                 }
-            }
 
-            const distinctUsers = Array.from(new Set(userLocationsArray.map(user => user.phone)))
-                .map(phone => {
-                    return {
-                        phone,
-                        name: userLocationsArray.find(user => user.phone === phone).name,
-                        role: userLocationsArray.find(user => user.phone === phone).role,
-                        affected: userLocationsArray.find(user => user.phone === phone).affected,
-                        default_loc: userLocationsArray.find(user => user.phone === phone).default_loc
-                    }
+                const distinctUsers = Array.from(new Set(userLocationsArray.map(user => user.phone)))
+                    .map(phone => {
+                        return {
+                            phone,
+                            name: userLocationsArray.find(user => user.phone === phone).name,
+                            role: userLocationsArray.find(user => user.phone === phone).role,
+                            affected: userLocationsArray.find(user => user.phone === phone).affected,
+                            default_loc: userLocationsArray.find(user => user.phone === phone).default_loc
+                        }
+                    });
+                    
+                return res.json({
+                    success: 'Notify users living within this location!',
+                    distinctUsersAffcted: distinctUsers
                 });
-
-            return res.json({
-                success: 'Notify users living within this location!',
-                // userLocationsAffected : userLocations,
-                distinctUsersAffcted: distinctUsers
-            });
-        }
-    });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
--- model/disaster.js
+++ model/disaster.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const DisasterSchema = mongoose.Schema({
     coordinates: {
         type: String
-    }
+    },
+    user_affected: [{
+        type: String
+    }]
 });
 
 module.exports = mongoose.model('disaster', DisasterSchema);
